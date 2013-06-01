540edf9010329c7b777767fa2c11d8a3cdcacfee
post <post@ec705bc8-faf6-4f9b-9aff-b4916e9bedc3>
2013-06-01 12:53:54 +0000

    Add support for compressed Samsung images.

--- RawSpeed/SrwDecoder.h
+++ RawSpeed/SrwDecoder.h
@@ -40,6 +40,7 @@ public:
   virtual void decodeMetaDataInternal(CameraMetaData *meta);
   virtual void checkSupportInternal(CameraMetaData *meta);
 private:
+  void SrwDecoder::decodeCompressed( TiffIFD* raw);
   TiffIFD *mRootIFD;
 };
 
