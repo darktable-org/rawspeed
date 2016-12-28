11bb322d7516d96882f87c7adce49b219147605c
Roman Lebedev <lebedev.ri@gmail.com>
2016-12-28 13:38:01 +0300

    CMake: better handle GoogleTest

--- cmake/Modules/GoogleTest.cmake
+++ cmake/Modules/GoogleTest.cmake
@@ -1,30 +1,47 @@
-cmake_minimum_required(VERSION 3.0)
+# Download and unpack googletest at configure time
+configure_file(${CMAKE_SOURCE_DIR}/cmake/Modules/GoogleTest.cmake.in ${CMAKE_BINARY_DIR}/googletest/CMakeLists.txt @ONLY)
 
-# Enable ExternalProject CMake module
-include(ExternalProject)
-
-# Download and install GoogleTest
-ExternalProject_Add(
-    googletest
-    URL /usr/src/googletest/
-    PREFIX ${CMAKE_BINARY_DIR}/googletest
-    # Disable install step
-    INSTALL_COMMAND ""
-    BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/googletest/src/googletest-build/googlemock/libgmock.a
+execute_process(
+  COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+  RESULT_VARIABLE result
+  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/googletest
 )
 
-# Create a libgoogletest target to be used as a dependency by test programs
-add_library(libgoogletest IMPORTED STATIC GLOBAL)
-add_dependencies(libgoogletest googletest)
+#message(WARNING "0 CMAKE_BINARY_DIR ${CMAKE_BINARY_DIR}")
 
-# Set googletest properties
-ExternalProject_Get_Property(googletest source_dir binary_dir)
+if(result)
+  message(FATAL_ERROR "CMake step for googletest failed: ${result}")
+endif()
 
-# avoid INTERFACE_INCLUDE_DIRECTORIES not found issue
-file(MAKE_DIRECTORY "${source_dir}/include")
+execute_process(
+  COMMAND ${CMAKE_COMMAND} --build .
+  RESULT_VARIABLE result
+  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/googletest
+)
 
-set_target_properties(libgoogletest PROPERTIES
-    "IMPORTED_LOCATION" "${binary_dir}/googlemock/libgmock.a"
-    "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
-    "INTERFACE_INCLUDE_DIRECTORIES" "${source_dir}/include"
-    )
+if(result)
+  message(FATAL_ERROR "Build step for googletest failed: ${result}")
+endif()
+
+# Prevent overriding the parent project's compiler/linker
+# settings on Windows
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
+set(CMAKE_C_FLAGS_SAVE "${CMAKE_C_FLAGS}")
+set(CMAKE_CXX_FLAGS_SAVE "${CMAKE_CXX_FLAGS}")
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -w")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -w")
+
+# Add googletest directly to our build. This defines
+# the gtest and gtest_main targets.
+add_subdirectory(${CMAKE_BINARY_DIR}/googletest/googletest-src
+                 ${CMAKE_BINARY_DIR}/googletest/googletest-build)
+
+set_target_properties(gtest PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES $<TARGET_PROPERTY:gtest,INTERFACE_INCLUDE_DIRECTORIES>)
+set_target_properties(gtest_main PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES $<TARGET_PROPERTY:gtest_main,INTERFACE_INCLUDE_DIRECTORIES>)
+set_target_properties(gmock PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES $<TARGET_PROPERTY:gmock,INTERFACE_INCLUDE_DIRECTORIES>)
+set_target_properties(gmock_main PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES $<TARGET_PROPERTY:gmock_main,INTERFACE_INCLUDE_DIRECTORIES>)
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS_SAVE}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS_SAVE}")
