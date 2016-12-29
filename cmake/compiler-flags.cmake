7c8e14ce3ffe0fdcb3cda28a431cfdc1536697c6
Roman Lebedev <lebedev.ri@gmail.com>
2016-12-29 12:22:36 +0300

    compiler-flags.cmake: check for C++11 first

--- cmake/compiler-flags.cmake
+++ cmake/compiler-flags.cmake
@@ -1,5 +1,10 @@
+include(CheckCXXCompilerFlag)
 include(CpuMarch)
 
+CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
+if(NOT COMPILER_SUPPORTS_CXX11)
+        message(FATAL_ERROR "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Please use a different C++ compiler.")
+endif()
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
 
 # always debug info
