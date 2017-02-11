acc15ae382c090b4d249b06aaa3c1079757b207e
Roman Lebedev <lebedev.ri@gmail.com>
2017-02-11 17:46:07 +0300

    Ci-script: understand GENERATOR and MAKEFLAGS options

--- .ci/ci-script.sh
+++ .ci/ci-script.sh
@@ -11,17 +11,20 @@
 
 set -ex
 
-PARALLEL="-j2"
+if [ -z "${MAKEFLAGS+x}" ];
+then
+  MAKEFLAGS="-j2 -v"
+fi
 
 target_build()
 {
   # to get as much of the issues into the log as possible
-  cmake --build "$BUILD_DIR" -- $PARALLEL -v || cmake --build "$BUILD_DIR" -- -j1 -v -k0
+  cmake --build "$BUILD_DIR" -- $MAKEFLAGS || cmake --build "$BUILD_DIR" -- -j1 -v -k0
 
   ctest --output-on-failure || ctest --rerun-failed -V -VV
 
   # and now check that it installs where told and only there.
-  cmake --build "$BUILD_DIR" --target install -- $PARALLEL -v || cmake --build "$BUILD_DIR" --target install -- -j1 -v -k0
+  cmake --build "$BUILD_DIR" --target install -- $MAKEFLAGS || cmake --build "$BUILD_DIR" --target install -- -j1 -v -k0
 }
 
 du -hcs "$SRC_DIR"
@@ -38,8 +41,14 @@ case "$FLAVOR" in
     ;;
 esac
 
+GENERATOR="Ninja"
+if [ ! -z "${G+x}" ];
+then
+  GENERATOR="$G"
+fi
+
 cd "$BUILD_DIR"
-cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" -GNinja -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" $ECO "$SRC_DIR" || (cat "$BUILD_DIR"/CMakeFiles/CMakeOutput.log; cat "$BUILD_DIR"/CMakeFiles/CMakeError.log)
+cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" -G"$GENERATOR" -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" $ECO "$SRC_DIR" || (cat "$BUILD_DIR"/CMakeFiles/CMakeOutput.log; cat "$BUILD_DIR"/CMakeFiles/CMakeError.log)
 
 case "$TARGET" in
   "build")
