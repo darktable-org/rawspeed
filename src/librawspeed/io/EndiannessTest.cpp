0255380e127f07904249be9c4321b29708c3d42e
Roman Lebedev <lebedev.ri@gmail.com>
2017-02-13 12:00:07 +0300

    EndiannessTest: dedup, use memcpy, struct, do print properly

--- src/librawspeed/io/EndiannessTest.cpp
+++ src/librawspeed/io/EndiannessTest.cpp
@@ -20,8 +20,11 @@
 
 #include "EndiannessTest.h"
 #include "common/Common.h" // for int32, short16, uint32, uint64, ushort16
-#include "io/Endianness.h" // for getHostEndianness, getByteSwapped, Endian...
-#include <gtest/gtest.h>   // for IsNullLiteralHelper, ParamGenerator, ASSE...
+#include "io/Endianness.h" // for getHostEndianness, Endianness::big, Endia...
+#include <cstring>         // for memcpy, memcmp
+#include <gtest/gtest.h>   // for Message, AssertionResult, ASSERT_PRED_FOR...
+#include <iomanip>         // for _Setw, setfill, setw, _Setfill
+#include <iostream>        // for hex, endl, ostream
 #include <utility>         // for get
 
 using namespace std;
@@ -31,8 +34,8 @@ using namespace RawSpeed;
 #!/bin/bash
 d=16 # squared, how many samples
 # B=2 # sizeof, bytes
-# b=x # print format
-# p="0x" # print prefix
+b=x # print format
+p="0x" # print prefix
 function proc {
   echo "$1" | od -A n --endian="$2" -t $3$B -N $B -w$B | tr -d ''
 }
@@ -47,10 +50,40 @@ do
     v=$(dd if=/dev/urandom bs=$B conv=sparse count=1 status=none)
     x=$(pp "$v" little);
     y=$(pp "$v" big);
-    echo "make_tuple($x, $y),";
+    echo "{$x, $y},";
   done;
 done;
 */
+
+#define setupHex setfill('0') << setw(2 * sizeof(T)) << std::hex
+
+template <typename T>
+::std::ostream& operator<<(::std::ostream& os, const intPair<T>& p) {
+  ::testing::Message msg;
+  msg << "(0x" << setupHex << p.first << ", 0x" << setupHex << p.second << ")";
+
+  return os << msg;
+}
+
+// no polymorphic lambda till c++14
+struct HexEquals {
+  template <typename T>
+  ::testing::AssertionResult operator()(const char* darg1, const char* darg2,
+                                        const T& arg1, const T& arg2) {
+    if (memcmp(&arg1, &arg2, sizeof(T)) == 0)
+      return ::testing::AssertionSuccess();
+
+    ::testing::Message msg;
+    msg << "      Expected: " << darg1 << std::endl;
+    msg << "      Which is: " << setupHex << arg1 << std::endl;
+    msg << "To be equal to: " << darg2 << std::endl;
+    msg << "      Which is: " << setupHex << arg2;
+
+    return ::testing::AssertionFailure() << msg;
+  }
+};
+
+#undef setupHex
 
 template <class T1, class T2>
 class AbstractGetByteSwappedTest : public ::testing::TestWithParam<T1> {
@@ -62,11 +95,11 @@ protected:
 
     // swap them around? the test is symmetrical
     if (std::tr1::get<1>(p)) {
-      in = std::tr1::get<0>(v);
-      expected = std::tr1::get<1>(v);
+      memcpy(&in, &(v.first), sizeof(T2));
+      memcpy(&expected, &(v.second), sizeof(T2));
     } else {
-      in = std::tr1::get<1>(v);
-      expected = std::tr1::get<0>(v);
+      memcpy(&in, &(v.first), sizeof(T2));
+      memcpy(&expected, &(v.second), sizeof(T2));
     }
   }
   T2 getByteSwappedT(const void* data, bool bswap) {
@@ -81,46 +114,48 @@ protected:
 
 /*
 B=2 # sizeof, bytes
-b=x # print format
-p="0x" # print prefix
 */
 class ushort16Test
     : public AbstractGetByteSwappedTest<ushort16TType, ushort16> {};
 INSTANTIATE_TEST_CASE_P(ushort16Test, ushort16Test,
                         ::testing::Combine(::testing::ValuesIn(ushort16Values),
                                            ::testing::Bool()));
-TEST_P(ushort16Test, swap) { ASSERT_EQ(getByteSwapped(in), expected); }
-TEST_P(ushort16Test, NOP) { ASSERT_EQ(getByteSwappedT(&in, false), in); }
+TEST_P(ushort16Test, swap) {
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwapped(in), expected);
+}
+TEST_P(ushort16Test, NOP) {
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwappedT(&in, false), in);
+}
 TEST_P(ushort16Test, typedSwap) {
-  ASSERT_EQ(getByteSwappedT(&in, true), expected);
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwappedT(&in, true), expected);
 }
 TEST_P(ushort16Test, get) {
   if (getHostEndianness() == little) {
-    ASSERT_EQ(getBEt(&in), expected);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getBEt(&in), expected);
   } else if (getHostEndianness() == big) {
-    ASSERT_EQ(getLEt(&in), expected);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getLEt(&in), expected);
   }
 }
 TEST_P(ushort16Test, getNOP) {
   if (getHostEndianness() == little) {
-    ASSERT_EQ(getLEt(&in), in);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getLEt(&in), in);
   } else if (getHostEndianness() == big) {
-    ASSERT_EQ(getBEt(&in), in);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getBEt(&in), in);
   }
 }
 
 TEST_P(ushort16Test, getU16) {
   if (getHostEndianness() == little) {
-    ASSERT_EQ(getU16BE(&in), expected);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getU16BE(&in), expected);
   } else if (getHostEndianness() == big) {
-    ASSERT_EQ(getU16LE(&in), expected);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getU16LE(&in), expected);
   }
 }
 TEST_P(ushort16Test, getU16NOP) {
   if (getHostEndianness() == little) {
-    ASSERT_EQ(getU16LE(&in), in);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getU16LE(&in), in);
   } else if (getHostEndianness() == big) {
-    ASSERT_EQ(getU16BE(&in), in);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getU16BE(&in), in);
   }
 }
 
@@ -129,67 +164,73 @@ class short16Test : public AbstractGetByteSwappedTest<ushort16TType, short16> {
 INSTANTIATE_TEST_CASE_P(short16Test, short16Test,
                         ::testing::Combine(::testing::ValuesIn(ushort16Values),
                                            ::testing::Bool()));
-TEST_P(short16Test, swap) { ASSERT_EQ(getByteSwapped(in), expected); }
-TEST_P(short16Test, NOP) { ASSERT_EQ(getByteSwappedT(&in, false), in); }
+TEST_P(short16Test, swap) {
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwapped(in), expected);
+}
+TEST_P(short16Test, NOP) {
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwappedT(&in, false), in);
+}
 TEST_P(short16Test, typedSwap) {
-  ASSERT_EQ(getByteSwappedT(&in, true), expected);
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwappedT(&in, true), expected);
 }
 TEST_P(short16Test, get) {
   if (getHostEndianness() == little) {
-    ASSERT_EQ(getBEt(&in), expected);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getBEt(&in), expected);
   } else if (getHostEndianness() == big) {
-    ASSERT_EQ(getLEt(&in), expected);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getLEt(&in), expected);
   }
 }
 TEST_P(short16Test, getNOP) {
   if (getHostEndianness() == little) {
-    ASSERT_EQ(getLEt(&in), in);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getLEt(&in), in);
   } else if (getHostEndianness() == big) {
-    ASSERT_EQ(getBEt(&in), in);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getBEt(&in), in);
   }
 }
 
 /*
 B=4 # sizeof, bytes
-b=x # print format
-p="0x" # print prefix
 */
 class uint32Test : public AbstractGetByteSwappedTest<uint32TType, uint32> {};
 INSTANTIATE_TEST_CASE_P(uint32Test, uint32Test,
                         ::testing::Combine(::testing::ValuesIn(uint32Values),
                                            ::testing::Bool()));
-TEST_P(uint32Test, swap) { ASSERT_EQ(getByteSwapped(in), expected); }
-TEST_P(uint32Test, NOP) { ASSERT_EQ(getByteSwappedT(&in, false), in); }
+TEST_P(uint32Test, swap) {
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwapped(in), expected);
+}
+TEST_P(uint32Test, NOP) {
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwappedT(&in, false), in);
+}
 TEST_P(uint32Test, typedSwap) {
-  ASSERT_EQ(getByteSwappedT(&in, true), expected);
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwappedT(&in, true), expected);
 }
 TEST_P(uint32Test, get) {
   if (getHostEndianness() == little) {
-    ASSERT_EQ(getBEt(&in), expected);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getBEt(&in), expected);
   } else if (getHostEndianness() == big) {
-    ASSERT_EQ(getLEt(&in), expected);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getLEt(&in), expected);
   }
 }
 TEST_P(uint32Test, getNOP) {
   if (getHostEndianness() == little) {
-    ASSERT_EQ(getLEt(&in), in);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getLEt(&in), in);
   } else if (getHostEndianness() == big) {
-    ASSERT_EQ(getBEt(&in), in);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getBEt(&in), in);
   }
 }
 
 TEST_P(uint32Test, getU32) {
   if (getHostEndianness() == little) {
-    ASSERT_EQ(getU32BE(&in), expected);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getU32BE(&in), expected);
   } else if (getHostEndianness() == big) {
-    ASSERT_EQ(getU32LE(&in), expected);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getU32LE(&in), expected);
   }
 }
 TEST_P(uint32Test, getU32NOP) {
   if (getHostEndianness() == little) {
-    ASSERT_EQ(getU32LE(&in), in);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getU32LE(&in), in);
   } else if (getHostEndianness() == big) {
-    ASSERT_EQ(getU32BE(&in), in);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getU32BE(&in), in);
   }
 }
 
@@ -197,109 +238,113 @@ class int32Test : public AbstractGetByteSwappedTest<uint32TType, int32> {};
 INSTANTIATE_TEST_CASE_P(int32Test, int32Test,
                         ::testing::Combine(::testing::ValuesIn(uint32Values),
                                            ::testing::Bool()));
-TEST_P(int32Test, swap) { ASSERT_EQ(getByteSwapped(in), expected); }
-TEST_P(int32Test, NOP) { ASSERT_EQ(getByteSwappedT(&in, false), in); }
+TEST_P(int32Test, swap) {
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwapped(in), expected);
+}
+TEST_P(int32Test, NOP) {
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwappedT(&in, false), in);
+}
 TEST_P(int32Test, typedSwap) {
-  ASSERT_EQ(getByteSwappedT(&in, true), expected);
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwappedT(&in, true), expected);
 }
 TEST_P(int32Test, get) {
   if (getHostEndianness() == little) {
-    ASSERT_EQ(getBEt(&in), expected);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getBEt(&in), expected);
   } else if (getHostEndianness() == big) {
-    ASSERT_EQ(getLEt(&in), expected);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getLEt(&in), expected);
   }
 }
 TEST_P(int32Test, getNOP) {
   if (getHostEndianness() == little) {
-    ASSERT_EQ(getLEt(&in), in);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getLEt(&in), in);
   } else if (getHostEndianness() == big) {
-    ASSERT_EQ(getBEt(&in), in);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getBEt(&in), in);
   }
 }
 
 /*
 B=8 # sizeof, bytes
-b=x # print format
-p="0x" # print prefix
 */
 class uint64Test : public AbstractGetByteSwappedTest<uint64TType, uint64> {};
 INSTANTIATE_TEST_CASE_P(uint64Test, uint64Test,
                         ::testing::Combine(::testing::ValuesIn(uint64Values),
                                            ::testing::Bool()));
-TEST_P(uint64Test, swap) { ASSERT_EQ(getByteSwapped(in), expected); }
-TEST_P(uint64Test, NOP) { ASSERT_EQ(getByteSwappedT(&in, false), in); }
+TEST_P(uint64Test, swap) {
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwapped(in), expected);
+}
+TEST_P(uint64Test, NOP) {
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwappedT(&in, false), in);
+}
 TEST_P(uint64Test, typedSwap) {
-  ASSERT_EQ(getByteSwappedT(&in, true), expected);
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwappedT(&in, true), expected);
 }
 TEST_P(uint64Test, get) {
   if (getHostEndianness() == little) {
-    ASSERT_EQ(getBEt(&in), expected);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getBEt(&in), expected);
   } else if (getHostEndianness() == big) {
-    ASSERT_EQ(getLEt(&in), expected);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getLEt(&in), expected);
   }
 }
 TEST_P(uint64Test, getNOP) {
   if (getHostEndianness() == little) {
-    ASSERT_EQ(getLEt(&in), in);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getLEt(&in), in);
   } else if (getHostEndianness() == big) {
-    ASSERT_EQ(getBEt(&in), in);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getBEt(&in), in);
   }
 }
 
-/*
-B=4 # sizeof, bytes
-b=f # print format
-p="" # print prefix
-*/
-class floatTest : public AbstractGetByteSwappedTest<floatTType, float> {};
+class floatTest : public AbstractGetByteSwappedTest<uint32TType, float> {};
 INSTANTIATE_TEST_CASE_P(floatTest, floatTest,
-                        ::testing::Combine(::testing::ValuesIn(floatValues),
+                        ::testing::Combine(::testing::ValuesIn(uint32Values),
                                            ::testing::Bool()));
-TEST_P(floatTest, swap) { ASSERT_EQ(getByteSwapped(in), expected); }
-TEST_P(floatTest, NOP) { ASSERT_EQ(getByteSwappedT(&in, false), in); }
+TEST_P(floatTest, swap) {
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwapped(in), expected);
+}
+TEST_P(floatTest, NOP) {
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwappedT(&in, false), in);
+}
 TEST_P(floatTest, typedSwap) {
-  ASSERT_EQ(getByteSwappedT(&in, true), expected);
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwappedT(&in, true), expected);
 }
 TEST_P(floatTest, get) {
   if (getHostEndianness() == little) {
-    ASSERT_EQ(getBEt(&in), expected);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getBEt(&in), expected);
   } else if (getHostEndianness() == big) {
-    ASSERT_EQ(getLEt(&in), expected);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getLEt(&in), expected);
   }
 }
 TEST_P(floatTest, getNOP) {
   if (getHostEndianness() == little) {
-    ASSERT_EQ(getLEt(&in), in);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getLEt(&in), in);
   } else if (getHostEndianness() == big) {
-    ASSERT_EQ(getBEt(&in), in);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getBEt(&in), in);
   }
 }
 
-/*
-B=8 # sizeof, bytes
-b=f # print format
-p="" # print prefix
-*/
-class doubleTest : public AbstractGetByteSwappedTest<doubleTType, double> {};
+class doubleTest : public AbstractGetByteSwappedTest<uint64TType, double> {};
 INSTANTIATE_TEST_CASE_P(doubleTest, doubleTest,
-                        ::testing::Combine(::testing::ValuesIn(doubleValues),
+                        ::testing::Combine(::testing::ValuesIn(uint64Values),
                                            ::testing::Bool()));
-TEST_P(doubleTest, swap) { ASSERT_EQ(getByteSwapped(in), expected); }
-TEST_P(doubleTest, NOP) { ASSERT_EQ(getByteSwappedT(&in, false), in); }
+TEST_P(doubleTest, swap) {
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwapped(in), expected);
+}
+TEST_P(doubleTest, NOP) {
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwappedT(&in, false), in);
+}
 TEST_P(doubleTest, typedSwap) {
-  ASSERT_EQ(getByteSwappedT(&in, true), expected);
+  ASSERT_PRED_FORMAT2(HexEquals{}, getByteSwappedT(&in, true), expected);
 }
 TEST_P(doubleTest, get) {
   if (getHostEndianness() == little) {
-    ASSERT_EQ(getBEt(&in), expected);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getBEt(&in), expected);
   } else if (getHostEndianness() == big) {
-    ASSERT_EQ(getLEt(&in), expected);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getLEt(&in), expected);
   }
 }
 TEST_P(doubleTest, getNOP) {
   if (getHostEndianness() == little) {
-    ASSERT_EQ(getLEt(&in), in);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getLEt(&in), in);
   } else if (getHostEndianness() == big) {
-    ASSERT_EQ(getBEt(&in), in);
+    ASSERT_PRED_FORMAT2(HexEquals{}, getBEt(&in), in);
   }
 }
