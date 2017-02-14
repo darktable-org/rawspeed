ee3e47d9c2a09e1861709b5eee646ef58de6d18d
Roman Lebedev <lebedev.ri@gmail.com>
2017-02-14 17:23:47 +0300

    Cr2Decoder: YUV_TO_RGB<0>: left shift of negative value
    ./Canon/EOS 40D/IMG_8951.CR2                           : starting decoding ...
    /home/lebedevri/rawspeed/src/librawspeed/decoders/Cr2Decoder.cpp:412:47: runtime error: left shift of negative value -4

--- src/librawspeed/decoders/Cr2Decoder.cpp
+++ src/librawspeed/decoders/Cr2Decoder.cpp
@@ -409,7 +409,7 @@ inline void YUV_TO_RGB<0>(int Y, int Cb, int Cr, const int* sraw_coeffs,
                           ushort16* X, int offset) {
   int r, g, b;
   r = sraw_coeffs[0] * (Y + Cr - 512);
-  g = sraw_coeffs[1] * (Y + ((-778 * Cb - (Cr << 11)) >> 12) - 512);
+  g = sraw_coeffs[1] * (Y + ((-778 * Cb - (Cr * 2048)) >> 12) - 512);
   b = sraw_coeffs[2] * (Y + (Cb - 512));
   STORE_RGB(X, r, g, b, offset);
 }
