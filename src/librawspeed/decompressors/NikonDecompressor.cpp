6617da15c8a3a398ff4dd6337af7abb482c3430a
Roman Lebedev <lebedev.ri@gmail.com>
2017-02-20 17:06:29 +0300

    NikonDecompressor: de-uglify nikon_tree, use proper 3D array

--- src/librawspeed/decompressors/NikonDecompressor.cpp
+++ src/librawspeed/decompressors/NikonDecompressor.cpp
@@ -31,25 +31,32 @@ using namespace std;
 
 namespace RawSpeed {
 
-static const uchar8 nikon_tree[][32] = {
-  { 0,1,5,1,1,1,1,1,1,2,0,0,0,0,0,0,	/* 12-bit lossy */
-  5,4,3,6,2,7,1,0,8,9,11,10,12 },
-  { 0,1,5,1,1,1,1,1,1,2,0,0,0,0,0,0,	/* 12-bit lossy after split */
-  0x39,0x5a,0x38,0x27,0x16,5,4,3,2,1,0,11,12,12 },
-  { 0,1,4,2,3,1,2,0,0,0,0,0,0,0,0,0,  /* 12-bit lossless */
-  5,4,6,3,7,2,8,1,9,0,10,11,12 },
-  { 0,1,4,3,1,1,1,1,1,2,0,0,0,0,0,0,	/* 14-bit lossy */
-  5,6,4,7,8,3,9,2,1,0,10,11,12,13,14 },
-  { 0,1,5,1,1,1,1,1,1,1,2,0,0,0,0,0,	/* 14-bit lossy after split */
-  8,0x5c,0x4b,0x3a,0x29,7,6,5,4,3,2,1,0,13,14 },
-  { 0,1,4,2,2,3,1,2,0,0,0,0,0,0,0,0,	/* 14-bit lossless */
-  7,6,8,5,9,4,10,3,11,12,2,0,1,13,14 } };
+static const uchar8 nikon_tree[][2][16] = {
+    {/* 12-bit lossy */
+     {0, 1, 5, 1, 1, 1, 1, 1, 1, 2, 0, 0, 0, 0, 0, 0},
+     {5, 4, 3, 6, 2, 7, 1, 0, 8, 9, 11, 10, 12}},
+    {/* 12-bit lossy after split */
+     {0, 1, 5, 1, 1, 1, 1, 1, 1, 2, 0, 0, 0, 0, 0, 0},
+     {0x39, 0x5a, 0x38, 0x27, 0x16, 5, 4, 3, 2, 1, 0, 11, 12, 12}},
+    {/* 12-bit lossless */
+     {0, 1, 4, 2, 3, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+     {5, 4, 6, 3, 7, 2, 8, 1, 9, 0, 10, 11, 12}},
+    {/* 14-bit lossy */
+     {0, 1, 4, 3, 1, 1, 1, 1, 1, 2, 0, 0, 0, 0, 0, 0},
+     {5, 6, 4, 7, 8, 3, 9, 2, 1, 0, 10, 11, 12, 13, 14}},
+    {/* 14-bit lossy after split */
+     {0, 1, 5, 1, 1, 1, 1, 1, 1, 1, 2, 0, 0, 0, 0, 0},
+     {8, 0x5c, 0x4b, 0x3a, 0x29, 7, 6, 5, 4, 3, 2, 1, 0, 13, 14}},
+    {/* 14-bit lossless */
+     {0, 1, 4, 2, 2, 3, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0},
+     {7, 6, 8, 5, 9, 4, 10, 3, 11, 12, 2, 0, 1, 13, 14}},
 
+};
 
 static HuffmanTable createHuffmanTable(uint32 huffSelect) {
   HuffmanTable ht;
-  uint32 count = ht.setNCodesPerLength(Buffer(nikon_tree[huffSelect], 16));
-  ht.setCodeValues(Buffer(nikon_tree[huffSelect]+16, count));
+  uint32 count = ht.setNCodesPerLength(Buffer(nikon_tree[huffSelect][0], 16));
+  ht.setCodeValues(Buffer(nikon_tree[huffSelect][1], count));
   ht.setup(true, false);
   return ht;
 }
