fdcad191f4d87e47f68e524cd0333581b7503f9c
Roman Lebedev <lebedev.ri@gmail.com>
2017-04-24 19:23:58 +0300

    libFuzzer_dummy_main.cpp: allow to dummy-run on the same sample set
    Helps visualise CORPUS coverage in non-fuzz build types

--- src/fuzzers/libFuzzer_dummy_main.cpp
+++ src/fuzzers/libFuzzer_dummy_main.cpp
@@ -18,13 +18,40 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
-#include <cstdlib>  // for EXIT_SUCCESS
-#include <iostream> // for cout
+#include "io/Buffer.h"     // for Buffer
+#include "io/FileReader.h" // for FileReader
+#include <cstdint>         // for uint8_t
+#include <cstdlib>         // for EXIT_SUCCESS, size_t
+#include <iostream>        // for operator<<, cout, ostream
+#include <memory>          // for unique_ptr
 
-int main() {
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size);
+
+static int usage() {
   std::cout << "This is just a placeholder.\nFor fuzzers to actually function, "
                "you need to build rawspeed with clang compiler, with FUZZ "
                "build type.\n";
+
+  return EXIT_SUCCESS;
+}
+
+static void process(const char* filename) {
+  rawspeed::FileReader reader(filename);
+
+  auto map(reader.readFile());
+
+  LLVMFuzzerTestOneInput(map->getData(0, map->getSize()), map->getSize());
+}
+
+int main(int argc, char** argv) {
+  if (1 == argc)
+    return usage();
+
+#ifdef _OPENMP
+#pragma omp parallel for default(shared) schedule(static)
+#endif
+  for (int i = 1; i < argc; ++i)
+    process(argv[i]);
 
   return EXIT_SUCCESS;
 }
