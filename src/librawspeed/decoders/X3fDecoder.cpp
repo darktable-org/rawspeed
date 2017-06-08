50f901272ab91d2fe5d49b88d805dae1a605dacf
Roman Lebedev <lebedev.ri@gmail.com>
2017-06-08 19:06:41 +0300

    X3fDecoder::createSigmaTable(): avoid left-shift of possibly-negative int

--- src/librawspeed/decoders/X3fDecoder.cpp
+++ src/librawspeed/decoders/X3fDecoder.cpp
@@ -340,7 +340,7 @@ void X3fDecoder::createSigmaTable(ByteStream *bytes_, int codes) {
         uint32 low_pos = 14-code_bits-val_bits;
         int v = (i >> low_pos) & ((1 << val_bits) - 1);
         v = HuffmanTable::signExtended(v, val_bits);
-        big_table[i] = (v<<8) | (code_bits+val_bits);
+        big_table[i] = (256 * v) | (code_bits + val_bits);
       } else {
         big_table[i] = 0xf;
       }
