97d2fc1920bae903dbba6e0d296fb37875f206ed
Roman Lebedev <lebedev.ri@gmail.com>
2017-11-23 10:50:43 +0300

    CMake: xmllint: also use add_executable(IMPORTED GLOBAL)

--- cmake/Modules/FindXMLLINT.cmake
+++ cmake/Modules/FindXMLLINT.cmake
@@ -1,9 +1,14 @@
-find_program(Xmllint_BIN xmllint)
+find_program(XMLLINT_EXECUTABLE
+  NAMES xmllint
+  DOC "The xmllint executable")
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(XMLLINT
   DEFAULT_MSG
-  Xmllint_BIN)
+  XMLLINT_EXECUTABLE)
+
+add_executable(xmllint IMPORTED GLOBAL)
+set_property(TARGET xmllint PROPERTY IMPORTED_LOCATION "${XMLLINT_EXECUTABLE}")
 
 SET_PACKAGE_PROPERTIES(XMLLINT PROPERTIES
   URL http://xmlsoft.org/
--- cmake/Modules/run-xmllint.cmake
+++ cmake/Modules/run-xmllint.cmake
@@ -1,22 +1,23 @@
 macro (check_xml XML XSD)
-  get_filename_component(FILENAME ${XML} NAME)
+  get_filename_component(FILENAME "${XML}" NAME)
 
   set(TOUCH "${CMAKE_CURRENT_BINARY_DIR}/${FILENAME}.touch")
 
   set(TMPNAME "validate-${FILENAME}")
 
   add_custom_command(
-    OUTPUT  ${TOUCH}
-    COMMAND ${Xmllint_BIN} --nonet --valid --noout --schema ${XSD} ${XML}
-    COMMAND ${CMAKE_COMMAND} -E touch ${TOUCH} # will be empty!
-    DEPENDS ${XML} ${XSD}
+    OUTPUT  "${TOUCH}"
+    COMMAND "$<TARGET_FILE:xmllint>" --nonet --valid --noout --schema "${XSD}" "${XML}"
+    COMMAND "${CMAKE_COMMAND}" -E touch "${TOUCH}" # will be empty!
+    DEPENDS xmllint "${XML}" "${XSD}"
     COMMENT "Checking validity of ${FILENAME}"
+    VERBATIM
   )
 
   add_custom_target(
     ${TMPNAME}
-    DEPENDS ${TOUCH} # will be empty!
-    DEPENDS ${XML}
+    DEPENDS "${TOUCH}" # will be empty!
+    DEPENDS "${XML}"
   )
 
   add_dependencies(check ${TMPNAME})
