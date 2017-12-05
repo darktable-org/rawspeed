b7248171be6292c9e17b3d11932d1fd19e57ff6b
Roman Lebedev <lebedev.ri@gmail.com>
2017-12-05 20:04:42 +0300

    DngOpcodes::PixelOpcode: enhance sanitization for planes.

--- src/librawspeed/common/DngOpcodes.cpp
+++ src/librawspeed/common/DngOpcodes.cpp
@@ -221,22 +221,21 @@ protected:
   explicit PixelOpcode(const RawImage& ri, ByteStream* bs) : ROIOpcode(ri, bs) {
     firstPlane = bs->getU32();
     planes = bs->getU32();
+
+    if (planes == 0 || firstPlane > ri->getCpp() || planes > ri->getCpp() ||
+        firstPlane + planes > ri->getCpp()) {
+      ThrowRDE("Bad plane params (first %u, num %u), got planes = %u",
+               firstPlane, planes, ri->getCpp());
+    }
+
     rowPitch = bs->getU32();
     colPitch = bs->getU32();
 
     const iRectangle2D& ROI = getRoi();
 
-    if (planes == 0)
-      ThrowRDE("Zero planes");
     if (rowPitch < 1 || rowPitch > static_cast<uint32>(ROI.getHeight()) ||
         colPitch < 1 || colPitch > static_cast<uint32>(ROI.getWidth()))
       ThrowRDE("Invalid pitch");
-  }
-
-  void setup(const RawImage& ri) override {
-    ROIOpcode::setup(ri);
-    if (firstPlane + planes > ri->getCpp())
-      ThrowRDE("Not that many planes in actual image");
   }
 
   // traverses the current ROI and applies the operation OP to each pixel,
