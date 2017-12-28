ab65ee6472616d6f7ae53f7dfd0f21fa36b704f4
Roman Lebedev <lebedev.ri@gmail.com>
2017-12-28 16:09:46 +0300

    Mark ThrowException() as noinline. No reason whatsoever to inline it.

--- src/librawspeed/common/RawspeedException.h
+++ src/librawspeed/common/RawspeedException.h
@@ -32,7 +32,7 @@
 namespace rawspeed {
 
 template <typename T>
-[[noreturn]] void __attribute__((noreturn, format(printf, 1, 2)))
+[[noreturn]] void __attribute__((noreturn, noinline, format(printf, 1, 2)))
 ThrowException(const char* fmt, ...) {
   static constexpr size_t bufSize = 8192;
 #if defined(HAVE_CXX_THREAD_LOCAL)
