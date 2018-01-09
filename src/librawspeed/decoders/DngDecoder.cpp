47b3303c7a921cd996657b88a6fd30c61fd4355c
Roman Lebedev <lebedev.ri@gmail.com>
2018-01-09 18:09:33 +0300

    DngDecoder::decodeBlackLevels(): BLACKLEVELDELTAV: better handle overflows

--- src/librawspeed/decoders/DngDecoder.cpp
+++ src/librawspeed/decoders/DngDecoder.cpp
@@ -717,9 +717,17 @@ bool DngDecoder::decodeBlackLevels(const TiffIFD* raw) {
     for (int i = 0; i < mRaw->dim.y; i++)
       black_sum[i&1] += blackleveldeltav->getFloat(i);
 
-    for (int i = 0; i < 4; i++)
-      mRaw->blackLevelSeparate[i] += static_cast<int>(
-          black_sum[i >> 1] / static_cast<float>(mRaw->dim.y) * 2.0F);
+    for (int i = 0; i < 4; i++) {
+      const float value =
+          black_sum[i >> 1] / static_cast<float>(mRaw->dim.y) * 2.0F;
+      if (value < std::numeric_limits<BlackType>::min() ||
+          value > std::numeric_limits<BlackType>::max())
+        ThrowRDE("Error decoding black level");
+
+      if (__builtin_sadd_overflow(mRaw->blackLevelSeparate[i], value,
+                                  &mRaw->blackLevelSeparate[i]))
+        ThrowRDE("Integer overflow when calculating black level");
+    }
   }
 
   if (raw->hasEntry(BLACKLEVELDELTAH)){
