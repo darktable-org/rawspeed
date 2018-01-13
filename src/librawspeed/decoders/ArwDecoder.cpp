774e87d87bea03af205aa27b6b6a20ab855ed95c
Roman Lebedev <lebedev.ri@gmail.com>
2018-01-13 14:31:12 +0300

    ArwDecoder::GetWB(): don't do dead memcpy().
    The very following SonyDecrypt() will override all that memory
    over again, and it won't even be read first.

--- src/librawspeed/decoders/ArwDecoder.cpp
+++ src/librawspeed/decoders/ArwDecoder.cpp
@@ -429,7 +429,6 @@ void ArwDecoder::GetWB() {
     const auto& ifd_crypt = priv->getRootIfdData();
     auto ifd_size = ifd_crypt.getSize();
     auto ifd_decoded = Buffer::Create(ifd_size);
-    memcpy(ifd_decoded.get(), ifd_crypt.begin(), ifd_size);
 
     SonyDecrypt(reinterpret_cast<const uint32*>(ifd_crypt.getData(off, len)),
                 reinterpret_cast<uint32*>(ifd_decoded.get() + off), len / 4,
