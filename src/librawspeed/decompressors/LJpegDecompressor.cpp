011039cbdcda1a37659003246a429433856cb7c0
Roman Lebedev <lebedev.ri@gmail.com>
2018-03-12 00:58:28 +0300

    LJpegDecompressor::decode(): sanitize off+dims.
    If the code was asked to fill such a tile, and
    it can't do that, let's not crash/fill only a part,
    but report as the proper error.

--- src/librawspeed/decompressors/LJpegDecompressor.cpp
+++ src/librawspeed/decompressors/LJpegDecompressor.cpp
@@ -59,6 +59,16 @@ void LJpegDecompressor::decode(uint32 offsetX, uint32 offsetY, uint32 width,
   if (offsetY >= static_cast<unsigned>(mRaw->dim.y))
     ThrowRDE("Y offset outside of image");
 
+  if (width > static_cast<unsigned>(mRaw->dim.x))
+    ThrowRDE("Tile wider than image");
+  if (height > static_cast<unsigned>(mRaw->dim.y))
+    ThrowRDE("Tile taller than image");
+
+  if (offsetX + width > static_cast<unsigned>(mRaw->dim.x))
+    ThrowRDE("Tile overflows image horizontally");
+  if (offsetY + height > static_cast<unsigned>(mRaw->dim.y))
+    ThrowRDE("Tile overflows image vertically");
+
   offX = offsetX;
   offY = offsetY;
   w = width;
