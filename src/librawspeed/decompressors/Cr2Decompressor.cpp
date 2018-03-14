87c26094076a8d93e3efcbb114c6b4e1c4904254
Roman Lebedev <lebedev.ri@gmail.com>
2018-03-14 13:30:27 +0300

    Cr2Decompressor::decode(): check that slice widths are positive.

--- src/librawspeed/decompressors/Cr2Decompressor.cpp
+++ src/librawspeed/decompressors/Cr2Decompressor.cpp
@@ -118,6 +118,11 @@ void Cr2Decompressor::decodeScan()
 void Cr2Decompressor::decode(std::vector<int> slicesWidths_)
 {
   slicesWidths = move(slicesWidths_);
+  for (auto slicesWidth : slicesWidths) {
+    if (slicesWidth <= 0)
+      ThrowRDE("Bad slice width: %i", slicesWidth);
+  }
+
   AbstractLJpegDecompressor::decode();
 }
 
