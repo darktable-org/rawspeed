eb99214e33cc6fcf2c9aae285d4e75358894a34d
Roman Lebedev <lebedev.ri@gmail.com>
2018-07-04 16:25:45 +0300

    Cr2Decompressor::decodeN_X_Y(): handle yet another unsufficient image size
    If we don't have enough slices, we won't fill the entire image area
    we were supposed to, so those pixels will be uninit garbage.
    
    I really hate this code. It might be the ugliest in the whole library.
    Other than the Fuji decompressor. This really needs to be rewritten,
    without all these branches nested deep within loops.

--- src/librawspeed/decompressors/Cr2Decompressor.cpp
+++ src/librawspeed/decompressors/Cr2Decompressor.cpp
@@ -208,6 +208,9 @@ void Cr2Decompressor::decodeN_X_Y()
       if (X_S_F == 1) {
         if (destX + sliceWidth > static_cast<unsigned>(mRaw->dim.x))
           ThrowRDE("Bad slice width / frame size / image size combination.");
+        if (((sliceId + 1) == slicing.numSlices) &&
+            ((destX + sliceWidth) < static_cast<unsigned>(mRaw->dim.x)))
+          ThrowRDE("Unsufficient slices - do not fill the entire image");
       } else {
         // FIXME.
       }
