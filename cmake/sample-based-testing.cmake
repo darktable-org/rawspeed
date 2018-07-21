1c993ecedd9966c17731d8d13a03b218860b3ff6
Roman Lebedev <lebedev.ri@gmail.com>
2018-07-21 20:13:50 +0300

    CMake: s/REFERENCE_SAMPLE_ARCHIVE/RAWSPEED_REFERENCE_SAMPLE_ARCHIVE

--- cmake/sample-based-testing.cmake
+++ cmake/sample-based-testing.cmake
@@ -1,14 +1,14 @@
 message(STATUS "Looking for sample set for sample-based testing")
-message(STATUS "Looking for sample set in ${REFERENCE_SAMPLE_ARCHIVE}")
-if(NOT (EXISTS "${REFERENCE_SAMPLE_ARCHIVE}"
-    AND EXISTS "${REFERENCE_SAMPLE_ARCHIVE}/filelist.sha1"
-    AND EXISTS "${REFERENCE_SAMPLE_ARCHIVE}/timestamp.txt"))
-  message(SEND_ERROR "Did not find sample set for sample-based testing! Either pass correct path in REFERENCE_SAMPLE_ARCHIVE, or disable RAWSPEED_ENABLE_SAMPLE_BASED_TESTING.")
+message(STATUS "Looking for sample set in ${RAWSPEED_REFERENCE_SAMPLE_ARCHIVE}")
+if(NOT (EXISTS "${RAWSPEED_REFERENCE_SAMPLE_ARCHIVE}"
+    AND EXISTS "${RAWSPEED_REFERENCE_SAMPLE_ARCHIVE}/filelist.sha1"
+    AND EXISTS "${RAWSPEED_REFERENCE_SAMPLE_ARCHIVE}/timestamp.txt"))
+  message(SEND_ERROR "Did not find sample set for sample-based testing! Either pass correct path in RAWSPEED_REFERENCE_SAMPLE_ARCHIVE, or disable RAWSPEED_ENABLE_SAMPLE_BASED_TESTING.")
 endif()
 
-message(STATUS "Found sample set in ${REFERENCE_SAMPLE_ARCHIVE}")
+message(STATUS "Found sample set in ${RAWSPEED_REFERENCE_SAMPLE_ARCHIVE}")
 
-file(STRINGS "${REFERENCE_SAMPLE_ARCHIVE}/filelist.sha1" _REFERENCE_SAMPLES ENCODING UTF-8)
+file(STRINGS "${RAWSPEED_REFERENCE_SAMPLE_ARCHIVE}/filelist.sha1" _REFERENCE_SAMPLES ENCODING UTF-8)
 
 set(REFERENCE_SAMPLES)
 set(REFERENCE_SAMPLE_HASHES)
@@ -20,7 +20,7 @@ foreach(STR ${_REFERENCE_SAMPLES})
   # Which for our purpose means:
   #   <40 chars><char><char><filename>    <- i.e. we just skip first 42 chars.
   string(SUBSTRING "${STR}" 42 -1 SAMPLENAME)
-  set(SAMPLENAME "${REFERENCE_SAMPLE_ARCHIVE}/${SAMPLENAME}")
+  set(SAMPLENAME "${RAWSPEED_REFERENCE_SAMPLE_ARCHIVE}/${SAMPLENAME}")
 
   if(NOT EXISTS "${SAMPLENAME}")
     message(SEND_ERROR "The sample \"${SAMPLENAME}\" does not exist!")
