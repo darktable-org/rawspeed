17611f3498ec858fa0a4d9a3a2180e96e4ca1641
Roman Lebedev <lebedev.ri@gmail.com>
2019-01-13 01:46:20 +0300

    OlympusDecompressor: mark SignBit() as 'inline', not that it really matters here.

--- src/librawspeed/decompressors/OlympusDecompressor.cpp
+++ src/librawspeed/decompressors/OlympusDecompressor.cpp
@@ -42,7 +42,7 @@ namespace {
 // in a horrible code. So let's just provide our own signbit(). It compiles to
 // the exact same code as the std::signbit(int).
 template <typename T, typename = std::enable_if_t<std::is_integral<T>::value>>
-constexpr __attribute__((const)) bool SignBit(T x) {
+inline constexpr __attribute__((const)) bool SignBit(T x) {
   return x < 0;
 }
 
