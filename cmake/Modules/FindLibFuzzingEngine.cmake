8906ca4a40da8387df8cbe2580397bd68777b035
Roman Lebedev <lebedev.ri@gmail.com>
2019-05-03 20:47:50 +0300

    CMake: adapt to LIB_FUZZING_ENGINE being any random string for linker
    Refs. https://github.com/google/oss-fuzz/issues/2317

--- cmake/Modules/FindLibFuzzingEngine.cmake
+++ cmake/Modules/FindLibFuzzingEngine.cmake
@@ -1,28 +1,20 @@
-# - Try to find LibFuzzingEngine
-# Once done, this will define
-#
-#  LibFuzzingEngine_FOUND - system has LibFuzzingEngine
-#  LibFuzzingEngine_LIBRARIES - link these to use LibFuzzingEngine
+# - Create LibFuzzingEngine imported target.
 
 include(LibFindMacros)
 
-if(EXISTS "${LIB_FUZZING_ENGINE}")
-  set(LibFuzzingEngine_LIBRARY "${LIB_FUZZING_ENGINE}")
-else()
-  set(LibFuzzingEngine_LIBRARY "LibFuzzingEngine_LIBRARY-NOTFOUND")
-endif()
-
-# Set the include dir variables and the libraries and let libfind_process do the rest.
-# NOTE: Singular variables for this library, plural for libraries this lib depends on.
-set(LibFuzzingEngine_PROCESS_LIBS LibFuzzingEngine_LIBRARY)
 libfind_process(LibFuzzingEngine)
 
-if(LibFuzzingEngine_FOUND)
-  add_library(LibFuzzingEngine INTERFACE IMPORTED)
-  set_property(TARGET LibFuzzingEngine PROPERTY INTERFACE_LINK_LIBRARIES "${LibFuzzingEngine_LIBRARIES}")
+add_library(LibFuzzingEngine INTERFACE IMPORTED)
+
+if(LIB_FUZZING_ENGINE)
+  # If LIB_FUZZING_ENGINE is specified, the compile-time flags were passed via CFLAGS / CXXFLAGS already.
+  set_property(TARGET LibFuzzingEngine PROPERTY INTERFACE_LINK_LIBRARIES "${LIB_FUZZING_ENGINE}")
+else()
+  set_property(TARGET LibFuzzingEngine PROPERTY INTERFACE_COMPILE_OPTIONS "-fsanitize=fuzzer-no-link")
+  set_property(TARGET LibFuzzingEngine PROPERTY INTERFACE_LINK_LIBRARIES  "-fsanitize=fuzzer")
 endif()
 
 set_package_properties(LibFuzzingEngine PROPERTIES
                        TYPE REQUIRED
-                       DESCRIPTION "A prebuilt fuzzing engine library (e.g. libFuzzer) that needs to be linked with all fuzz target"
+                       DESCRIPTION "A prebuilt fuzzing engine library (e.g. libFuzzer.a, or -fsanitize=fuzzer) that needs to be linked with all fuzz target"
                        PURPOSE "Used to actually drive the fuzz targets")
