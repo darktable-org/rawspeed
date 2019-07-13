4421e4c1ec19577cbdd3444b6bd6b09b75f77bc8
Roman Lebedev <lebedev.ri@gmail.com>
2019-07-13 17:40:43 +0300

    Fix GCC OpenMP-less build

--- src/librawspeed/common/Mutex.h
+++ src/librawspeed/common/Mutex.h
@@ -77,17 +77,17 @@ public:
   // Acquire/lock this mutex exclusively.  Only one thread can have exclusive
   // access at any one time.  Write operations to guarded data require an
   // exclusive lock.
-  void __attribute__((const)) Lock() const ACQUIRE() {
+  void Lock() const ACQUIRE() {
     // NOP, since there is no mutex. only here to still check for proper locking
   }
 
   // Release/unlock an exclusive mutex.
-  void __attribute__((const)) Unlock() const RELEASE() {
+  void Unlock() const RELEASE() {
     // NOP, since there is no mutex. only here to still check for proper locking
   }
 
   // Try to acquire the mutex.  Returns true on success, and false on failure.
-  bool __attribute__((const)) TryLock() const TRY_ACQUIRE(true) {
+  bool TryLock() const TRY_ACQUIRE(true) {
     // NOP, since there is no mutex. only here to still check for proper locking
     return true;
   }
