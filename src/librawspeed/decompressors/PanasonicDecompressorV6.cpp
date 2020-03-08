308cd0d91c07fa7a7b066d115b5f8d3b6e8c7d1e
Roman Lebedev <lebedev.ri@gmail.com>
2020-03-08 17:47:59 +0300

    PanasonicDecompressorV6: simplify pana_cs6_page_decoder (-4%)
    Now that we construct it once per block, all that cruft is pointless.

--- src/librawspeed/decompressors/PanasonicDecompressorV6.cpp
+++ src/librawspeed/decompressors/PanasonicDecompressorV6.cpp
@@ -34,41 +34,32 @@ namespace rawspeed {
 namespace {
 struct pana_cs6_page_decoder {
   std::array<unsigned int, 14> pixelbuffer;
-  unsigned lastoffset, maxoffset;
-  unsigned char current;
-  const unsigned char* buffer;
-  pana_cs6_page_decoder(const unsigned char* _buffer, unsigned int bsize)
-      : lastoffset(0), maxoffset(bsize), current(0), buffer(_buffer) {}
-  void read_page(); // will throw IO error if not enough space in buffer
+  unsigned char current = 0;
+
+  explicit pana_cs6_page_decoder(ByteStream bs) {
+    auto wbuffer = [&](int i) {
+      return static_cast<uint16_t>(bs.peekByte(15 - i));
+    };
+    pixelbuffer[0] = (wbuffer(0) << 6) | (wbuffer(1) >> 2); // 14 bit
+    pixelbuffer[1] =
+        (((wbuffer(1) & 0x3) << 12) | (wbuffer(2) << 4) | (wbuffer(3) >> 4)) &
+        0x3fff;
+    pixelbuffer[2] = (wbuffer(3) >> 2) & 0x3;
+    pixelbuffer[3] = ((wbuffer(3) & 0x3) << 8) | wbuffer(4);
+    pixelbuffer[4] = (wbuffer(5) << 2) | (wbuffer(6) >> 6);
+    pixelbuffer[5] = ((wbuffer(6) & 0x3f) << 4) | (wbuffer(7) >> 4);
+    pixelbuffer[6] = (wbuffer(7) >> 2) & 0x3;
+    pixelbuffer[7] = ((wbuffer(7) & 0x3) << 8) | wbuffer(8);
+    pixelbuffer[8] = ((wbuffer(9) << 2) & 0x3fc) | (wbuffer(10) >> 6);
+    pixelbuffer[9] = ((wbuffer(10) << 4) | (wbuffer(11) >> 4)) & 0x3ff;
+    pixelbuffer[10] = (wbuffer(11) >> 2) & 0x3;
+    pixelbuffer[11] = ((wbuffer(11) & 0x3) << 8) | wbuffer(12);
+    pixelbuffer[12] = (((wbuffer(13) << 2) & 0x3fc) | wbuffer(14) >> 6) & 0x3ff;
+    pixelbuffer[13] = ((wbuffer(14) << 4) | (wbuffer(15) >> 4)) & 0x3ff;
+  }
+
   unsigned int nextpixel() { return current < 14 ? pixelbuffer[current++] : 0; }
 };
-
-void pana_cs6_page_decoder::read_page() {
-  if (!buffer || (maxoffset - lastoffset < 16))
-    ThrowRDE("Input failure");
-  auto wbuffer = [&](int i) {
-    return static_cast<uint16_t>(buffer[lastoffset + 15 - i]);
-  };
-  pixelbuffer[0] = (wbuffer(0) << 6) | (wbuffer(1) >> 2); // 14 bit
-  pixelbuffer[1] =
-      (((wbuffer(1) & 0x3) << 12) | (wbuffer(2) << 4) | (wbuffer(3) >> 4)) &
-      0x3fff;
-  pixelbuffer[2] = (wbuffer(3) >> 2) & 0x3;
-  pixelbuffer[3] = ((wbuffer(3) & 0x3) << 8) | wbuffer(4);
-  pixelbuffer[4] = (wbuffer(5) << 2) | (wbuffer(6) >> 6);
-  pixelbuffer[5] = ((wbuffer(6) & 0x3f) << 4) | (wbuffer(7) >> 4);
-  pixelbuffer[6] = (wbuffer(7) >> 2) & 0x3;
-  pixelbuffer[7] = ((wbuffer(7) & 0x3) << 8) | wbuffer(8);
-  pixelbuffer[8] = ((wbuffer(9) << 2) & 0x3fc) | (wbuffer(10) >> 6);
-  pixelbuffer[9] = ((wbuffer(10) << 4) | (wbuffer(11) >> 4)) & 0x3ff;
-  pixelbuffer[10] = (wbuffer(11) >> 2) & 0x3;
-  pixelbuffer[11] = ((wbuffer(11) & 0x3) << 8) | wbuffer(12);
-  pixelbuffer[12] = (((wbuffer(13) << 2) & 0x3fc) | wbuffer(14) >> 6) & 0x3ff;
-  pixelbuffer[13] = ((wbuffer(14) << 4) | (wbuffer(15) >> 4)) & 0x3ff;
-#undef wbuffer
-  current = 0;
-  lastoffset += 16;
-}
 } // namespace
 
 PanasonicDecompressorV6::PanasonicDecompressorV6(const RawImage& img,
@@ -93,8 +84,8 @@ void PanasonicDecompressorV6::decompressBlock(ByteStream* rowInput, int row,
                                               int col) const {
   const Array2DRef<uint16_t> out(mRaw->getU16DataAsUncroppedArray2DRef());
 
-  pana_cs6_page_decoder page(rowInput->getData(16), 16);
-  page.read_page();
+  pana_cs6_page_decoder page(rowInput->getStream(16));
+
   std::array<unsigned int, 2> oddeven = {0, 0};
   std::array<unsigned int, 2> nonzero = {0, 0};
   unsigned pmul = 0;
