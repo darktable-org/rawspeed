aa0ff2ae208e49fb2e2751c85a8d39b195040e60
Roman Lebedev <lebedev.ri@gmail.com>
2021-06-17 19:51:57 +0300

    RawImageDataFloat::scaleValues(): drop dead manually-vectorized version
    I've verified that the code in question is nicely autovectorized.
    The dropped code was never even compiled.

--- src/librawspeed/common/RawImageDataFloat.cpp
+++ src/librawspeed/common/RawImageDataFloat.cpp
@@ -143,146 +143,28 @@ RawImageDataFloat::RawImageDataFloat() {
     startWorker(RawImageWorker::SCALE_VALUES, true);
 }
 
-#if 0 // def WITH_SSE2
-
-  void RawImageDataFloat::scaleValues(int start_y, int end_y) {
-    bool WITH_SSE2;
-#ifdef _MSC_VER
-    int info[4];
-    __cpuid(info, 1);
-    WITH_SSE2 = !!(info[3]&(1 << 26));
-#else
-    WITH_SSE2 = true;
-#endif
-
-    float app_scale = 65535.0F / (whitePoint - blackLevelSeparate[0]);
-    // Check SSE2
-    if (WITH_SSE2 && app_scale < 63) {
-
-      __m128i sseround;
-      __m128i ssesub2;
-      __m128i ssesign;
-      auto* sub_mul = alignedMallocArray<uint32_t, 16, __m128i>(4);
-	  if (!sub_mul)
-		ThrowRDE("Out of memory, failed to allocate 128 bytes");
-
-      uint32_t gw = pitch / 16;
-      // 10 bit fraction
-      uint32_t mul = (int)(1024.0F * 65535.0F / (float)(whitePoint - blackLevelSeparate[mOffset.x&1]));
-      mul |= ((int)(1024.0F * 65535.0F / (float)(whitePoint - blackLevelSeparate[(mOffset.x+1)&1])))<<16;
-      uint32_t b = blackLevelSeparate[mOffset.x&1] | (blackLevelSeparate[(mOffset.x+1)&1]<<16);
-
-      for (int i = 0; i< 4; i++) {
-        sub_mul[i] = b;     // Subtract even lines
-        sub_mul[4+i] = mul;   // Multiply even lines
-      }
-
-      mul = (int)(1024.0F * 65535.0F / (float)(whitePoint - blackLevelSeparate[2+(mOffset.x&1)]));
-      mul |= ((int)(1024.0F * 65535.0F / (float)(whitePoint - blackLevelSeparate[2+((mOffset.x+1)&1)])))<<16;
-      b = blackLevelSeparate[2+(mOffset.x&1)] | (blackLevelSeparate[2+((mOffset.x+1)&1)]<<16);
-
-      for (int i = 0; i< 4; i++) {
-        sub_mul[8+i] = b;   // Subtract odd lines
-        sub_mul[12+i] = mul;  // Multiply odd lines
-      }
-
-      sseround = _mm_set_epi32(512, 512, 512, 512);
-      ssesub2 = _mm_set_epi32(32768, 32768, 32768, 32768);
-      ssesign = _mm_set_epi32(0x80008000, 0x80008000, 0x80008000, 0x80008000);
-
-      for (int y = start_y; y < end_y; y++) {
-        __m128i* pixel = (__m128i*) & data[(mOffset.y+y)*pitch];
-        __m128i ssescale, ssesub;
-        if (((y+mOffset.y)&1) == 0) {
-          ssesub = _mm_load_si128((__m128i*)&sub_mul[0]);
-          ssescale = _mm_load_si128((__m128i*)&sub_mul[4]);
-        } else {
-          ssesub = _mm_load_si128((__m128i*)&sub_mul[8]);
-          ssescale = _mm_load_si128((__m128i*)&sub_mul[12]);
-        }
-
-        for (uint32_t x = 0 ; x < gw; x++) {
-          __m128i pix_high;
-          __m128i temp;
-          _mm_prefetch((char*)(pixel+1), _MM_HINT_T0);
-          __m128i pix_low = _mm_load_si128(pixel);
-          // Subtract black
-          pix_low = _mm_subs_epu16(pix_low, ssesub);
-          // Multiply the two unsigned shorts and combine it to 32 bit result
-          pix_high = _mm_mulhi_epu16(pix_low, ssescale);
-          temp = _mm_mullo_epi16(pix_low, ssescale);
-          pix_low = _mm_unpacklo_epi16(temp, pix_high);
-          pix_high = _mm_unpackhi_epi16(temp, pix_high);
-          // Add rounder
-          pix_low = _mm_add_epi32(pix_low, sseround);
-          pix_high = _mm_add_epi32(pix_high, sseround);
-          // Shift down
-          pix_low = _mm_srai_epi32(pix_low, 10);
-          pix_high = _mm_srai_epi32(pix_high, 10);
-          // Subtract to avoid clipping
-          pix_low = _mm_sub_epi32(pix_low, ssesub2);
-          pix_high = _mm_sub_epi32(pix_high, ssesub2);
-          // Pack
-          pix_low = _mm_packs_epi32(pix_low, pix_high);
-          // Shift sign off
-          pix_low = _mm_xor_si128(pix_low, ssesign);
-          _mm_store_si128(pixel, pix_low);
-          pixel++;
-        }
-      }
-      alignedFree(sub_mul);
-    } else {
-      // Not SSE2
-      int gw = dim.x * cpp;
-      int mul[4];
-      int sub[4];
-      for (int i = 0; i < 4; i++) {
-        int v = i;
-        if ((mOffset.x&1) != 0)
-          v ^= 1;
-        if ((mOffset.y&1) != 0)
-          v ^= 2;
-        mul[i] = (int)(16384.0F * 65535.0F / (float)(whitePoint - blackLevelSeparate[v]));
-        sub[i] = blackLevelSeparate[v];
-      }
-      for (int y = start_y; y < end_y; y++) {
-        uint16_t *pixel = (uint16_t*)getData(0, y);
-        int *mul_local = &mul[2*(y&1)];
-        int *sub_local = &sub[2*(y&1)];
-        for (int x = 0 ; x < gw; x++) {
-          pixel[x] = clampBits(((pixel[x] - sub_local[x&1]) * mul_local[x&1] + 8192) >> 14, 16);
-        }
-      }
+void RawImageDataFloat::scaleValues(int start_y, int end_y) {
+  int gw = dim.x * cpp;
+  std::array<float, 4> mul;
+  std::array<float, 4> sub;
+  for (int i = 0; i < 4; i++) {
+    int v = i;
+    if ((mOffset.x & 1) != 0)
+      v ^= 1;
+    if ((mOffset.y & 1) != 0)
+      v ^= 2;
+    mul[i] = 65535.0F / static_cast<float>(whitePoint - blackLevelSeparate[v]);
+    sub[i] = static_cast<float>(blackLevelSeparate[v]);
+  }
+  for (int y = start_y; y < end_y; y++) {
+    auto* pixel = reinterpret_cast<float*>(getData(0, y));
+    float* mul_local = &mul[2 * (y & 1)];
+    float* sub_local = &sub[2 * (y & 1)];
+    for (int x = 0; x < gw; x++) {
+      pixel[x] = (pixel[x] - sub_local[x & 1]) * mul_local[x & 1];
     }
   }
-
-#else
-
-  void RawImageDataFloat::scaleValues(int start_y, int end_y) {
-    int gw = dim.x * cpp;
-    std::array<float, 4> mul;
-    std::array<float, 4> sub;
-    for (int i = 0; i < 4; i++) {
-      int v = i;
-      if ((mOffset.x&1) != 0)
-        v ^= 1;
-      if ((mOffset.y&1) != 0)
-        v ^= 2;
-      mul[i] =
-          65535.0F / static_cast<float>(whitePoint - blackLevelSeparate[v]);
-      sub[i] = static_cast<float>(blackLevelSeparate[v]);
-    }
-    for (int y = start_y; y < end_y; y++) {
-      auto* pixel = reinterpret_cast<float*>(getData(0, y));
-      float *mul_local = &mul[2*(y&1)];
-      float *sub_local = &sub[2*(y&1)];
-      for (int x = 0 ; x < gw; x++) {
-        pixel[x] = (pixel[x] - sub_local[x&1]) * mul_local[x&1];
-      }
-    }
-  }
-
-#endif
+}
 
   /* This performs a 4 way interpolated pixel */
   /* The value is interpolated from the 4 closest valid pixels in */
