cacac041115e841d5ff1c5e52b433099ee2e896e
Roman Lebedev <lebedev.ri@gmail.com>
2022-10-22 19:40:04 +0300

    `RawImageData::unpoisonPadding()`: port to `Array2DRef`

--- src/librawspeed/common/RawImage.cpp
+++ src/librawspeed/common/RawImage.cpp
@@ -143,12 +143,10 @@ void RawImageData::unpoisonPadding() const {
   if (padding <= 0)
     return;
 
+  const Array2DRef<std::byte> img = getByteDataAsUncroppedArray2DRef();
   for (int j = 0; j < uncropped_dim.y; j++) {
-    const uint8_t* const curr_line_end =
-        getDataUncropped(uncropped_dim.x - 1, j) + bpp;
-
     // and now unpoison the padding.
-    ASan::UnPoisonMemoryRegion(curr_line_end, padding);
+    ASan::UnPoisonMemoryRegion(&img(j, img.width - 1) + 1, padding);
   }
 }
 #else
