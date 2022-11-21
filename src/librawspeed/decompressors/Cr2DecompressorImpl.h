3eaec34e4002f444ff950b3fd81d79c98680c62f
Roman Lebedev <lebedev.ri@gmail.com>
2022-11-21 19:15:05 +0300

    Cr2Decompressor: don't forget to check that image actually has positive area

--- src/librawspeed/decompressors/Cr2DecompressorImpl.h
+++ src/librawspeed/decompressors/Cr2DecompressorImpl.h
@@ -140,21 +140,6 @@ Cr2Decompressor<HuffmanTable>::Cr2Decompressor(
   if (mRaw->getCpp() != 1 || mRaw->getBpp() != sizeof(uint16_t))
     ThrowRDE("Unexpected cpp: %u", mRaw->getCpp());
 
-  if (!mRaw->dim.x || !mRaw->dim.y || mRaw->dim.x > 19440 ||
-      mRaw->dim.y > 5920) {
-    ThrowRDE("Unexpected image dimensions found: (%u; %u)", mRaw->dim.x,
-             mRaw->dim.y);
-  }
-
-  if (!frame.hasPositiveArea())
-    ThrowRDE("LJPEG Frame size is wrong");
-
-  for (auto sliceId = 0; sliceId < slicing.numSlices; sliceId++) {
-    const auto sliceWidth = slicing.widthOfSlice(sliceId);
-    if (sliceWidth <= 0)
-      ThrowRDE("Bad slice width: %i", sliceWidth);
-  }
-
   if (!((std::make_tuple(3, 2, 2) == format) ||
         (std::make_tuple(3, 2, 1) == format) ||
         (std::make_tuple(2, 1, 1) == format) ||
@@ -164,21 +149,33 @@ Cr2Decompressor<HuffmanTable>::Cr2Decompressor(
 
   const Dsc dsc(format);
 
+  dim = mRaw->dim;
+  if (!dim.hasPositiveArea() || dim.x % dsc.groupSize != 0)
+    ThrowRDE("Unexpected image dimension multiplicity");
+  dim.x /= dsc.groupSize;
+
+  if (!frame.hasPositiveArea() || frame.x % dsc.X_S_F != 0 ||
+      frame.y % dsc.Y_S_F != 0)
+    ThrowRDE("Unexpected LJpeg frame dimension multiplicity");
+  frame.x /= dsc.X_S_F;
+  frame.y /= dsc.Y_S_F;
+
+  if (mRaw->dim.x > 19440 || mRaw->dim.y > 5920) {
+    ThrowRDE("Unexpected image dimensions found: (%u; %u)", mRaw->dim.x,
+             mRaw->dim.y);
+  }
+
+  for (auto sliceId = 0; sliceId < slicing.numSlices; sliceId++) {
+    const auto sliceWidth = slicing.widthOfSlice(sliceId);
+    if (sliceWidth <= 0)
+      ThrowRDE("Bad slice width: %i", sliceWidth);
+  }
+
   if (dsc.subSampled == mRaw->isCFA)
     ThrowRDE("Cannot decode subsampled image to CFA data or vice versa");
 
   if (static_cast<int>(rec.size()) != dsc.N_COMP)
     ThrowRDE("HT/Initial predictor count does not match component count");
-
-  dim = mRaw->dim;
-  if (dim.x % dsc.groupSize != 0)
-    ThrowRDE("Unexpected image dimension multiplicity");
-  dim.x /= dsc.groupSize;
-
-  if (frame.x % dsc.X_S_F != 0 || frame.y % dsc.Y_S_F != 0)
-    ThrowRDE("Unexpected LJpeg frame dimension multiplicity");
-  frame.x /= dsc.X_S_F;
-  frame.y /= dsc.Y_S_F;
 
   for (const auto& recip : rec) {
     if (!recip.ht.isFullDecode())
