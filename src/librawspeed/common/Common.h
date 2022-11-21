2a25c821e033d72435cd191506a953f708a83336
Roman Lebedev <lebedev.ri@gmail.com>
2022-11-21 20:35:18 +0300

    `bit_cast<>()`: use newer type traits

--- src/librawspeed/common/Common.h
+++ src/librawspeed/common/Common.h
@@ -58,12 +58,11 @@ inline void copyPixels(uint8_t* dest, int dstPitch, const uint8_t* src,
   }
 }
 
-template <
-    typename T_TO, typename T_FROM,
-    typename = std::enable_if_t<sizeof(T_TO) == sizeof(T_FROM)>,
-    typename = std::enable_if_t<std::is_trivially_constructible<T_TO>::value>,
-    typename = std::enable_if_t<std::is_trivially_copyable<T_TO>::value>,
-    typename = std::enable_if_t<std::is_trivially_copyable<T_FROM>::value>>
+template <typename T_TO, typename T_FROM,
+          typename = std::enable_if_t<sizeof(T_TO) == sizeof(T_FROM)>,
+          typename = std::enable_if_t<std::is_trivially_constructible_v<T_TO>>,
+          typename = std::enable_if_t<std::is_trivially_copyable_v<T_TO>>,
+          typename = std::enable_if_t<std::is_trivially_copyable_v<T_FROM>>>
 inline T_TO bit_cast(const T_FROM& from) noexcept {
   T_TO to;
   memcpy(&to, &from, sizeof(T_TO));
