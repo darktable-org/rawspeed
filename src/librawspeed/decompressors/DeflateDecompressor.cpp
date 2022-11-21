4e66629dae1487218786e96cf226f7e806cce79d
Roman Lebedev <lebedev.ri@gmail.com>
2022-11-21 22:37:49 +0300

    DeflateDecompressor: decodeFPDeltaRow(): pass CroppedArray2DRef by const-ref

--- src/librawspeed/decompressors/DeflateDecompressor.cpp
+++ src/librawspeed/decompressors/DeflateDecompressor.cpp
@@ -85,9 +85,9 @@ template <> struct StorageType<ieee_754_2008::Binary32> {
 };
 
 template <typename T>
-static inline void decodeFPDeltaRow(const unsigned char* src,
-                                    size_t realTileWidth,
-                                    CroppedArray2DRef<float> out, int row) {
+static inline void
+decodeFPDeltaRow(const unsigned char* src, size_t realTileWidth,
+                 const CroppedArray2DRef<float>& out, int row) {
   using storage_type = typename StorageType<T>::type;
   constexpr unsigned storage_bytes = sizeof(storage_type);
   constexpr unsigned bytesps = T::StorageWidth / 8;
