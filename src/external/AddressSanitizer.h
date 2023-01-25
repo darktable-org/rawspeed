587bb2e281edd6f96981d5aee3a5df1f681fed3c
Roman Lebedev <lebedev.ri@gmail.com>
2023-01-25 20:09:33 +0300

    [ASan] `PoisonMemoryRegion()`: pointer-to-byte

--- src/external/AddressSanitizer.h
+++ src/external/AddressSanitizer.h
@@ -46,7 +46,7 @@ struct ASan final {
   ~ASan() = delete;
 
   // Marks memory region [addr, addr+size) as unaddressable.
-  static void PoisonMemoryRegion(void const volatile* addr, size_t size);
+  static void PoisonMemoryRegion(const volatile std::byte* addr, size_t size);
   // Marks memory region [addr, addr+size) as addressable.
   static void UnPoisonMemoryRegion(void const volatile* addr, size_t size);
 
@@ -56,7 +56,8 @@ struct ASan final {
 };
 
 #if __has_feature(address_sanitizer) || defined(__SANITIZE_ADDRESS__)
-inline void ASan::PoisonMemoryRegion(void const volatile* addr, size_t size) {
+inline void ASan::PoisonMemoryRegion(const volatile std::byte* addr,
+                                     size_t size) {
   __asan_poison_memory_region(addr, size);
 }
 inline void ASan::UnPoisonMemoryRegion(void const volatile* addr, size_t size) {
@@ -67,8 +68,9 @@ inline bool ASan::RegionIsPoisoned(void const volatile* addr, size_t size) {
   return nullptr != __asan_region_is_poisoned(beg, size);
 }
 #else
-inline void ASan::PoisonMemoryRegion([[maybe_unused]] void const volatile* addr,
-                                     [[maybe_unused]] size_t size) {
+inline void
+ASan::PoisonMemoryRegion([[maybe_unused]] const volatile std::byte* addr,
+                         [[maybe_unused]] size_t size) {
   // If we are building without ASan, then there is no way to have a non-empty
   // body of this function. It's better than to have a macros, or to use
   // preprocessor in every place it is called.
