cfc36593628b95fd79e228336ee1b7ab3b7aac72
Roman Lebedev <lebedev.ri@gmail.com>
2023-02-22 19:36:21 +0300

    Rw2Decoder::decodeRawInternal(): just use `decode12BitRawUnpackedLeftAligned<little>`

--- src/librawspeed/decoders/Rw2Decoder.cpp
+++ src/librawspeed/decoders/Rw2Decoder.cpp
@@ -96,7 +96,7 @@ RawImage Rw2Decoder::decodeRawInternal() {
     if (size >= width * height * 2) {
       // It's completely unpacked little-endian
       mRaw->createData();
-      u.decodeRawUnpacked<12, Endianness::little>(width, height);
+      u.decode12BitRawUnpackedLeftAligned<Endianness::little>(width, height);
     } else if (size >= width * height * 3 / 2) {
       // It's a packed format
       mRaw->createData();
