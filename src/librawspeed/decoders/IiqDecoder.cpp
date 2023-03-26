06d52f97d30b50fcfff31517fbb9952fc52796ab
Roman Lebedev <lebedev.ri@gmail.com>
2023-03-26 04:03:37 +0300

    `IiqDecoder::PhaseOneFlatField`: use `roundUpDivision()`

--- src/librawspeed/decoders/IiqDecoder.cpp
+++ src/librawspeed/decoders/IiqDecoder.cpp
@@ -417,8 +417,8 @@ void IiqDecoder::PhaseOneFlatField(ByteStream data, IiqCorr corr) const {
   if (head[2] == 0 || head[3] == 0 || head[4] == 0 || head[5] == 0)
     return;
 
-  int wide = head[2] / head[4] + (head[2] % head[4] != 0);
-  int high = head[3] / head[5] + (head[3] % head[5] != 0);
+  int wide = roundUpDivision(head[2], head[4]);
+  int high = roundUpDivision(head[3], head[5]);
 
   std::vector<float> mrow_storage;
   Array2DRef<float> mrow = Array2DRef<float>::create(
