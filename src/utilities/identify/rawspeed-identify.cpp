4616c379de520922ef9d6c219752333f335288af
Roman Lebedev <lebedev.ri@gmail.com>
2023-11-29 03:10:57 +0300

    rs-identify: `FileReader` already widens filenames internally

--- src/utilities/identify/rawspeed-identify.cpp
+++ src/utilities/identify/rawspeed-identify.cpp
@@ -28,14 +28,6 @@
 #include <sys/stat.h>
 #include <vector>
 
-#ifdef _WIN32
-#ifndef NOMINMAX
-#define NOMINMAX // do not want the min()/max() macros!
-#endif
-
-#include <Windows.h>
-#endif
-
 namespace rawspeed::identify {
 
 std::string find_cameras_xml(const char* argv0);
@@ -145,27 +137,9 @@ int main(int argc, char* argv[]) { // NOLINT
       return 2;
     }
 
-#ifndef _WIN32
-    char* imageFileName = argv[1];
-#else
-    // turn the locale ANSI encoded string into UTF-8 so that FileReader can
-    // turn it into UTF-16 later
-    int size = MultiByteToWideChar(CP_ACP, 0, argv[1], -1, nullptr, 0);
-    std::wstring wImageFileName;
-    wImageFileName.resize(size);
-    MultiByteToWideChar(CP_ACP, 0, argv[1], -1, &wImageFileName[0], size);
-    size = WideCharToMultiByte(CP_UTF8, 0, &wImageFileName[0], -1, nullptr, 0,
-                               nullptr, nullptr);
-    std::string _imageFileName;
-    _imageFileName.resize(size);
-    char* imageFileName = &_imageFileName[0];
-    WideCharToMultiByte(CP_UTF8, 0, &wImageFileName[0], -1, imageFileName, size,
-                        nullptr, nullptr);
-#endif
+    fprintf(stderr, "Loading file: \"%s\"\n", argv[1]);
 
-    fprintf(stderr, "Loading file: \"%s\"\n", imageFileName);
-
-    FileReader f(imageFileName);
+    FileReader f(argv[1]);
 
     auto [storage, buf] = f.readFile();
 
