8ca0e57ea17318567e91558967f335119433fe8f
Roman Lebedev <lebedev.ri@gmail.com>
2023-12-22 04:27:41 +0300

    MD5: getAsFullBufferRef(): plain reference is enough here.

--- src/utilities/rstest/md5.h
+++ src/utilities/rstest/md5.h
@@ -162,7 +162,7 @@ public:
 
   [[nodiscard]] FullBufferRef getAsFullBufferRef() const {
     return std::visit(
-        [](auto&& arg) -> FullBufferRef {
+        [](const auto& arg) -> FullBufferRef {
           using T = std::decay_t<decltype(arg)>;
           if constexpr (std::is_same_v<T, FullBufferRef>)
             return arg;
