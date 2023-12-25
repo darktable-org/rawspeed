f59f0ba8fbdbbf810c6f07d9f9794adfd6e99c23
Roman Lebedev <lebedev.ri@gmail.com>
2023-12-25 21:07:19 +0300

    CMake: add `SET_PACKAGE_PROPERTIES(RawSpeedClangTidyModule ...`

--- cmake/clang-tidy.cmake
+++ cmake/clang-tidy.cmake
@@ -7,6 +7,12 @@ if(USE_RAWSPEED_CLANG_TIDY_MODULE)
 
   find_package(RawSpeedClangTidyModule ${CLANG_TIDY_VERSION_MAJOR}.0.0...<${CLANG_TIDY_VERSION_MAJOR_NEXT}.0.0 REQUIRED CONFIG)
 
+  SET_PACKAGE_PROPERTIES(RawSpeedClangTidyModule PROPERTIES
+    URL https://github.com/darktable-org/rawspeed-clang-tidy-module
+    DESCRIPTION "custom clang-tidy module for RawSpeed library"
+    PURPOSE "RawSpeed-specific clang-tidy checks"
+  )
+
   set(plugin "--load=$<TARGET_PROPERTY:RawSpeedClangTidyModule::clangTidyRawSpeedModule,LOCATION>")
 endif()
 
