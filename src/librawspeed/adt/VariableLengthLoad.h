8c8c9b68f7b17a8cc15d3e990b11566a5e557e2d
Roman Lebedev <lebedev.ri@gmail.com>
2024-01-05 01:46:01 +0300

    Extract `variableLengthLoadNaiveViaMemcpy()` out of `BitStreamForwardSequentialReplenisher::getInput()`
    It is not obvious if this snippet is the best solution to the problem,
    but even if it is, there's hidden UB there:
    `Base::data + Base::pos` may be creating OOB pointer,
    even if the size (`bytesRemaining`) will be zero then.

---
+++ src/librawspeed/adt/VariableLengthLoad.h
@@ -0,0 +1,55 @@
+/*
+    RawSpeed - RAW file decoder.
+
+    Copyright (C) 2024 Roman Lebedev
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+#pragma once
+
+#include "adt/Array1DRef.h"
+#include "adt/Invariant.h"
+#include <algorithm>
+#include <cstdint>
+#include <cstring>
+
+namespace rawspeed {
+
+inline void variableLengthLoadNaiveViaMemcpy(Array1DRef<uint8_t> out,
+                                             Array1DRef<const uint8_t> in,
+                                             int inPos) {
+  invariant(out.size() != 0);
+  invariant(in.size() != 0);
+  invariant(out.size() <= in.size());
+  invariant(inPos >= 0);
+
+  std::fill(out.begin(), out.end(), 0);
+
+  // How many bytes are left in input buffer?
+  // Since pos can be past-the-end we need to carefully handle overflow.
+  int bytesRemaining = (inPos < in.size()) ? in.size() - inPos : 0;
+  // And if we are not at the end of the input, we may have more than we need.
+  bytesRemaining = std::min<int>(out.size(), bytesRemaining);
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpragmas"
+#pragma GCC diagnostic ignored "-Wunknown-warning-option"
+#pragma GCC diagnostic ignored "-Wunsafe-buffer-usage"
+  memcpy(out.begin(), in.begin() + inPos, bytesRemaining);
+#pragma GCC diagnostic pop
+}
+
+} // namespace rawspeed
