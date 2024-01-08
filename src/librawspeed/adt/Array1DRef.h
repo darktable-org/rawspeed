e06541ea900acb1e01c7741c7f839f20e2b1bf2a
Roman Lebedev <lebedev.ri@gmail.com>
2024-01-08 20:39:12 +0300

    Array1DRef: we are never cted with a `nullptr`.

--- src/librawspeed/adt/Array1DRef.h
+++ src/librawspeed/adt/Array1DRef.h
@@ -93,8 +93,8 @@ template <typename T> Array1DRef(T* data_, int numElts_) -> Array1DRef<T>;
 template <class T>
 Array1DRef<T>::Array1DRef(T* data_, const int numElts_)
     : data(data_), numElts(numElts_) {
+  invariant(data);
   invariant(numElts >= 0);
-  invariant(data || numElts == 0);
 }
 
 template <class T>
