351578d4e1eb5dfad4110fa6985110683c8f4f16
Roman Lebedev <lebedev.ri@gmail.com>
2024-02-12 03:59:52 +0300

    CMake: when debug info is enabled, also enable it's compression
    This results in -48% less footprint of the Release build directory...

--- cmake/debug-info.cmake
+++ cmake/debug-info.cmake
@@ -1,7 +1,17 @@
+include(CheckCompilerFlag)
+include(CheckLinkerFlag)
+
 if(RAWSPEED_ENABLE_DEBUG_INFO)
   # always debug info
   add_definitions(-g3)
   add_definitions(-ggdb3)
+
+  check_compiler_flag(CXX -gz COMPILER_SUPPORTS_DEBUG_INFO_COMPRESSION)
+  check_linker_flag(CXX -gz COMPILER_SUPPORTS_DEBUG_INFO_COMPRESSION_LINK)
+  if(COMPILER_SUPPORTS_DEBUG_INFO_COMPRESSION AND COMPILER_SUPPORTS_DEBUG_INFO_COMPRESSION_LINK)
+    add_compile_options(-gz)
+    add_link_options(-gz)
+  endif()
 elseif(NOT RAWSPEED_ENABLE_DEBUG_INFO)
   add_definitions(-g0)
 else()
