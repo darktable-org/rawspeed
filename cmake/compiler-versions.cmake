9e75a4d80aaebcd26c128c5da227f441e0467233
Roman Lebedev <lebedev.ri@gmail.com>
2024-02-16 02:00:05 +0300

    CMake: also check version of C++ standard library's implementation

--- cmake/compiler-versions.cmake
+++ cmake/compiler-versions.cmake
@@ -57,3 +57,60 @@ endif()
 if(CMAKE_OSX_DEPLOYMENT_TARGET AND CMAKE_OSX_DEPLOYMENT_TARGET VERSION_LESS 12.5)
   message(SEND_ERROR "Targeting OSX version ${CMAKE_OSX_DEPLOYMENT_TARGET} older than 12.5 is unsupported.")
 endif()
+
+include(CheckCXXSourceCompiles)
+
+if(NOT DEFINED RAWSPEED_LIBSTDCXX_MIN)
+  set(LIBSTDCXX_MIN 12)
+  message(STATUS "Performing libstdc++ version check")
+  file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/libstdcxx_version_check.cpp"
+  "#include <version>
+  #define STR_HELPER(x) #x
+  #define STR(x) STR_HELPER(x)
+  #if defined(__GLIBCXX__)
+  #if !defined(_GLIBCXX_RELEASE) || _GLIBCXX_RELEASE < ${LIBSTDCXX_MIN}
+  #pragma message(\"Unsupported libstdc++ version: \" STR(_GLIBCXX_RELEASE))
+  #error
+  #endif
+  #endif
+  int main() { return 0; }
+  ")
+  try_compile(RAWSPEED_LIBSTDCXX_MIN
+  "${CMAKE_CURRENT_BINARY_DIR}/libstdcxx_version_check"
+  "${CMAKE_CURRENT_BINARY_DIR}/libstdcxx_version_check.cpp"
+  OUTPUT_VARIABLE MSG)
+  if(NOT RAWSPEED_LIBSTDCXX_MIN)
+    message(SEND_ERROR ${MSG})
+  else()
+    message(STATUS "Performing libstdc++ version check - Success")
+  endif()
+endif()
+
+if(NOT DEFINED RAWSPEED_LIBCXX_MIN)
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+    set(LIBCXX_MIN 13000)
+    # NOTE: with libc++-16, version gains extra tailing zero
+  else()
+    set(LIBCXX_MIN 14000)
+  endif()
+  message(STATUS "Performing libc++ version check")
+  file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/libcpp_version_check.cpp"
+  "#include <version>
+  #define STR_HELPER(x) #x
+  #define STR(x) STR_HELPER(x)
+  #if defined(_LIBCPP_VERSION) && _LIBCPP_VERSION < ${LIBCXX_MIN}
+  #pragma message(\"Unsupported libc++ version: \" STR(_LIBCPP_VERSION))
+  #error
+  #endif
+  int main() { return 0; }
+  ")
+  try_compile(RAWSPEED_LIBCXX_MIN
+  "${CMAKE_CURRENT_BINARY_DIR}/libcpp_version_check"
+  "${CMAKE_CURRENT_BINARY_DIR}/libcpp_version_check.cpp"
+  OUTPUT_VARIABLE MSG)
+  if(NOT RAWSPEED_LIBCXX_MIN)
+    message(SEND_ERROR ${MSG})
+  else()
+    message(STATUS "Performing libc++ version check - Success")
+  endif()
+endif()
